1fbcb0dfe972ecdd9fbc5e02fbebe63c7b1712c9
sumitpatel0718 <sumitpatelreso@gmail.com>
2026-02-12 23:48:42 +0530

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main(){
-    printf("need to make concurrent data structures like map , queue, priority queue etc. \n"); return 0;
-    
+    printf("in this issue we are working on concurrent data structures like map, queue, priority queue etc. \n"); return 0;
+
 }
